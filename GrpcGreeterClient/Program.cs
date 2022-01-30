e1d6befb9c95f514eb3f6c8b4536535c210011db
Nitin Vaswani <nitin.vaswani@rediffmail.com>
2022-01-30 14:01:54 +0530

    Add project files.

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7021");
+var client = new Greeter.GreeterClient(channel);
+var helloRequest = new HelloRequest { Name = "Nitin" };
+var reply = await client.SayHelloAsync(helloRequest);
+Console.WriteLine($"Greeting :{reply.Message}");
+
+var greetReply = await client.GreetWithTimeAsync(helloRequest);
+Console.WriteLine($"{greetReply.GreetingOfDay}\nToday is {greetReply.Day}, {greetReply.Time}\n{greetReply.ExtraMessage}");
+Console.ReadKey();
+
